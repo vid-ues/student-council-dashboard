8befba8cb6e1a94b8295f5b68d1784594d6e2730
vid-ues <tubtimcharoons@sfusd.edu>
2024-09-26 00:13:52 -0700

    Updates

---
+++ precache-manifest.d16014b0d020e9e34b3f287769b14cc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34937d858efea7e4967cb10c3a9b016d",
+    "url": "/student-council-dashboard/index.html"
+  },
+  {
+    "revision": "cf2411d1d96b6ffc1d83",
+    "url": "/student-council-dashboard/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "75cbd2f7b68548a1a587",
+    "url": "/student-council-dashboard/static/js/2.a7d2e0aa.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/student-council-dashboard/static/js/2.a7d2e0aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb4c1d966e15b70d4f75",
+    "url": "/student-council-dashboard/static/js/3.2341272e.chunk.js"
+  },
+  {
+    "revision": "cf2411d1d96b6ffc1d83",
+    "url": "/student-council-dashboard/static/js/main.fce7b037.chunk.js"
+  },
+  {
+    "revision": "9350c84d499b851ddfc9",
+    "url": "/student-council-dashboard/static/js/runtime-main.67cf3ae7.js"
+  }
+]);
